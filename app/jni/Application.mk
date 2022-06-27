80944fc60281c354db3a848f48a5b9a170a3dc1c
jinan <neil.liu@easemob.com>
2022-06-27 11:50:58 +0800

    Use NDK19c and modify .so name from sqlcipher to cipherdb

--- app/jni/Application.mk
+++ app/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 #APP_ABI := arm64-v8a x86_64
-APP_STL := gnustl_static #GNU STL
 APP_CPPFLAGS +=-std=c++11 #允许使用c++11的函数等功能
+APP_STL := c++_static
 LOCAL_MULTILIB := 32
-APP_PLATFORM=android-11
+APP_PLATFORM=android-21
